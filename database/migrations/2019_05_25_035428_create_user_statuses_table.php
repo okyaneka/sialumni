a815d911b4783053a3ddaf78faca8451cba07ec6
Kulo Oky <okyaneka@gmail.com>
2019-06-18 20:07:29 +0700

    rev user_statuses table
    add nullable to info and year

--- database/migrations/2019_05_25_035428_create_user_statuses_table.php
+++ database/migrations/2019_05_25_035428_create_user_statuses_table.php
@@ -16,13 +16,18 @@ class CreateUserStatusesTable extends Migration
         Schema::create('user_statuses', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned();
-            // $table->foreign('user_id')->references('id')->on('users');
             $table->bigInteger('status_id')->unsigned();
-            // $table->foreign('status_id')->references('id')->on('statuses');
-            $table->string('info');
-            $table->string('year');
+            $table->string('info')->nullable();
+            $table->string('year')->nullable();
 
             $table->unique(['user_id', 'status_id']);
+
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->foreign('status_id')
+                  ->references('id')->on('statuses')
+                  ->onDelete('cascade');
         });
     }
 

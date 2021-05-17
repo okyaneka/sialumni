77d72b62076b55b5e77e7f39ce93dad7dc7eb6ba
Oky <okyaneka@gmail.com>
2021-05-17 09:43:13 +0700

    Fix db status

--- resources/views/statuses/create.blade.php
+++ resources/views/statuses/create.blade.php
@@ -22,7 +22,7 @@
                             @csrf
 
                             <div class="pl-lg-4">
-                                <div class="form-group{{ $errors->has('status') ? ' has-danger' : '' }}">
+                                <div class="form-group">
                                     <label class="form-control-label" for="input-status">{{ __('Status Lulusan') }}</label>
                                     <input type="text" name="status" id="input-status" class="form-control form-control-alternative{{ $errors->has('status') ? ' is-invalid' : '' }}" placeholder="{{ __('Status Lulusan') }}" value="{{ old('status') }}" required>
 

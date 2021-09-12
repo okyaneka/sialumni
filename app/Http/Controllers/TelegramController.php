8e77906547368b90a5d426ebad4dd2dfa266b3c2
Kulooky <okyaneka@gmail.com>
2021-09-12 18:37:08 +0700

    Add feature bot for group (#17)

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -46,55 +46,52 @@ class TelegramController extends Controller
 
     function info(BotMan $bot)
     {
-        if ($bot->getMessage()->getPayload()['chat']['type'] == 'group') {
-            $bot->reply("Hai : " . $bot->getUser()->getFirstname() . ".\nSilahkan check pesan pribadi ya");
-        }
-
+        $this->isGroup($bot);
         return $bot->startConversation(new \App\Conversations\InfoConversation(), $bot->getUser()->getId(), TelegramDriver::class);
     }
 
     public function validasi(BotMan $bot)
     {
-        if ($bot->getMessage()->getPayload()['chat']['type'] == 'group') {
-            $bot->reply("Hai : " . $bot->getUser()->getFirstname() . ".\nSilahkan check pesan pribadi ya");
-        }
-
+        $this->isGroup($bot);
         return $bot->startConversation(new \App\Conversations\ValidasiConversation(), $bot->getUser()->getId(), TelegramDriver::class);
     }
 
     public function update(BotMan $bot)
     {
-        if ($bot->getMessage()->getPayload()['chat']['type'] == 'group') {
-            $bot->reply("Hai : " . $bot->getUser()->getFirstname() . ".\nSilahkan check pesan pribadi ya");
-        }
-
+        $this->isGroup($bot);
         return $bot->startConversation(new \App\Conversations\UpdateConversation(), $bot->getUser()->getId(), TelegramDriver::class);
     }
 
     public function infoloker(BotMan $bot)
     {
-        if ($bot->getMessage()->getPayload()['chat']['type'] == 'group') {
-            $bot->reply("Hai : " . $bot->getUser()->getFirstname() . ".\nSilahkan check pesan pribadi ya");
-        }
-
+        $this->isGroup($bot);
         return $bot->startConversation(new \App\Conversations\LokerConversation(), $bot->getUser()->getId(), TelegramDriver::class);
     }
 
     public function infoalumni(BotMan $bot)
     {
-        if ($bot->getMessage()->getPayload()['chat']['type'] == 'group') {
-            $bot->reply("Hai : " . $bot->getUser()->getFirstname() . ".\nSilahkan check pesan pribadi ya");
-        }
-
+        $this->isGroup($bot);
         return $bot->startConversation(new \App\Conversations\AlumniConversation(), $bot->getUser()->getId(), TelegramDriver::class);
     }
 
     function carialumni(BotMan $bot)
     {
-        if ($bot->getMessage()->getPayload()['chat']['type'] == 'group') {
-            $bot->reply("Hai : " . $bot->getUser()->getFirstname() . ".\nSilahkan check pesan pribadi ya");
-        }
-
+        $this->isGroup($bot);
         return $bot->startConversation(new \App\Conversations\FindAlumniConversation(), $bot->getUser()->getId(), TelegramDriver::class);
+    }
+
+    private function isGroup(BotMan $bot)
+    {
+        $payload = $bot->getMessage()->getPayload();
+        if ($payload['chat']['type'] == 'group') {
+            $name = !empty($payload['from']['username']) ? '@' . $payload['from']['username'] : $payload['from']['first_name'];
+            if (User::where('telegram_id', $payload['from']['id'])->count()) {
+                $bot->reply("Hai $name.\nSilahkan check pesan pribadi ya");
+            } else {
+                $bot->reply("Hai $name.\nCoba /start Aku dulu. Lewat japri ya...");
+            } 
+            return true;
+        }
+        return false;
     }
 }
